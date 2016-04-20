4a5fadc79558656386fd02c9ffe2b97cc1e4eb45
Richard Neil Ilagan <me@richardneililagan.com>
2016-04-20 16:50:40 +0800

    Initial commit

---
+++ brew.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env sh
+
+# taps
+brew tap homebrew/fuse
+brew tap homebrew/services
+brew tap homebrew/versions
+brew tap rstacruz/backup
+
+# packages
+brew install ack
+brew install autoconf
+brew install automake
+brew install cscope
+brew install freetype
+brew install gdbm
+brew install gettext
+brew install git
+brew install gnupg
+brew install imagemagick
+brew install jpeg
+brew install libevent
+brew install libgpg-error
+brew install libksba
+brew install libpng
+brew install libtiff
+brew install libtool
+brew install libyaml
+brew install mackup
+brew install macvim
+brew install openssl
+brew install pcre
+brew install pkg-config
+brew install postgresql
+brew install postgresql94
+brew install python
+brew install readline
+brew install sqlite
+brew install the_silver_searcher
+brew install tig
+brew install tmux
+brew install xz
---
+++ cask.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env sh
+
+# taps
+brew tap caskroom/cask
+brew tap caskroom/fonts
+
+# casks
+brew cask install android-file-transfer
+brew cask install boot2docker
+brew cask install dropbox
+brew cask install font-anonymous-pro-for-powerline
+brew cask install font-dejavu-sans-mono-for-powerline
+brew cask install font-droid-sans-mono-for-powerline
+brew cask install font-fira-mono-for-powerline
+brew cask install font-inconsolata-for-powerline
+brew cask install font-liberation-mono-for-powerline
+brew cask install font-menlo-for-powerline
+brew cask install font-sauce-code-powerline
+brew cask install font-ubuntu-mono-powerline
+brew cask install google-chrome
+brew cask install istat-menus
+brew cask install iterm2
+brew cask install kaleidoscope
+brew cask install keeweb
+brew cask install ngrok
+brew cask install slack
+brew cask install spectacle
+brew cask install spotify
+brew cask install steam
+brew cask install techstoreclub-simple-comic
+brew cask install viber
+brew cask install vlc
